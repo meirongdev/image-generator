81dece8279b40c3cfeed6e9fc07c3e113f56e1e6
Meirong <meirongdev@gmail.com>
2024-09-06 10:39:12 +0800

    feat: add image list

--- cmd/main.go
+++ cmd/main.go
@@ -1,18 +1,34 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	"github.com/meirongdev/image-generator/internal/drawing"
 )
 
 func router() *gin.Engine {
 	r := gin.Default()
-	userRoute := r.Group("/image")
+	r.LoadHTMLGlob("templates/*.html")
+	imageGroup := r.Group("/image")
 	{
-		userRoute.GET("/circles", func(c *gin.Context) {
-			file := drawing.DrawOne("circles")
+		imageGroup.GET("/:name", func(c *gin.Context) {
+			name := c.Param("name")
+			file := drawing.DrawOne(name)
 			c.Header("Content-Type", "image/png")
 			c.File(file)
+		})
+	}
+	listGroup := r.Group("/list")
+	{
+		var keys []string
+		for k := range drawing.DRAWINGS {
+			keys = append(keys, k)
+		}
+		listGroup.GET("/", func(c *gin.Context) {
+			c.HTML(http.StatusOK, "bootstrap5.html", gin.H{
+				"keys": keys,
+			})
 		})
 	}
 	return r
